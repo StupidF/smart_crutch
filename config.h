15ca2ae5e3cb3676bc781caaa5d92d95a1dba776
StupidF <44726848+StupidF@users.noreply.github.com>
2019-11-24 17:35:42 +0800

    头文件

---
+++ config.h
@@ -0,0 +1,15 @@
+#ifndef		__CONFIG_H
+#define		__CONFIG_H
+
+#include "STC12C5A60S2.h" 
+
+typedef   unsigned char  u8;
+typedef   unsigned int   u16;
+typedef   unsigned long  u32;
+
+typedef   unsigned char  uchar;
+typedef   unsigned int   uint;
+typedef   unsigned long  ulint;
+
+#endif
+
